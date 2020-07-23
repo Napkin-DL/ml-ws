434479b2a7f4faf3b3cc5d8fc1f9597c7a196cf5
EC2 Default User <ec2-user@ip-172-16-37-23.us-east-2.compute.internal>
2020-07-23 15:51:36 +0000

    update-0723

---
+++ 3.Distributed_training_and_EIA_Deploy_for_Pytorch/src_dir/main_trainer.py
@@ -0,0 +1,396 @@
+
+import argparse
+import json
+import logging
+import os
+import random
+import sys
+import time
+import warnings
+from functools import partial
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+import dis_util
+import sagemaker_containers
+import util
+
+## Apex import package
+try:
+    from apex.parallel import DistributedDataParallel as DDP
+    from apex.fp16_utils import *
+    from apex import amp, optimizers
+    from apex.multi_tensor_apply import multi_tensor_applier
+except ImportError:
+    raise ImportError(
+        "Please install apex from https://www.github.com/nvidia/apex to run this example.")
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def parser_args():
+    parser = argparse.ArgumentParser()
+
+    # Default Setting
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--backend', type=str, default='nccl',
+                        help='backend for distributed training (tcp, gloo on cpu and gloo, nccl on gpu)')
+    parser.add_argument('--channels-last', type=bool, default=True)
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('-p', '--print-freq', default=10, type=int,
+                        metavar='N', help='print frequency (default: 10)')
+
+    # Hyperparameter Setting
+    parser.add_argument('--model_name', type=str, default='resnet50')
+    parser.add_argument('--height', type=int, default=128)
+    parser.add_argument('--width', type=int, default=128)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--num_epochs', type=int, default=3)
+    parser.add_argument('--batch-size', type=int, default=64)
+    parser.add_argument('--test-batch-size', type=int, default=200, metavar='N',
+                        help='input batch size for testing (default: 200)')
+
+    # APEX Setting for Distributed Training
+    parser.add_argument('--apex', type=bool, default=False)
+    parser.add_argument('--opt-level', type=str, default='O0')
+    parser.add_argument('--keep-batchnorm-fp32', type=str, default=None)
+    parser.add_argument('--loss-scale', type=str, default=None)
+    parser.add_argument('--sync_bn', action='store_true',
+                        help='enabling apex sync BN.')
+    parser.add_argument('--prof', default=-1, type=int,
+                        help='Only run 10 iterations for profiling.')
+
+    # SageMaker Container environment
+    parser.add_argument('--hosts', type=list,
+                        default=json.loads(os.environ['SM_HOSTS']))
+    parser.add_argument('--current-host', type=str,
+                        default=os.environ['SM_CURRENT_HOST'])
+    parser.add_argument('--model-dir', type=str,
+                        default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--data-dir', type=str,
+                        default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--num-gpus', type=int,
+                        default=os.environ['SM_NUM_GPUS'])
+    parser.add_argument('--output_data_dir', type=str,
+                        default=os.environ.get('SM_OUTPUT_DATA_DIR'))
+
+    args = parser.parse_args()
+    return args
+
+
+def _get_train_data_loader(args, **kwargs):
+    logger.info("Get train data loader")
+    transform = transforms.Compose([
+        transforms.Resize((args.width, args.height)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ColorJitter(brightness=0, contrast=0, saturation=0, hue=0),
+        transforms.RandomAffine(
+            0, translate=None, scale=None, shear=None, resample=False, fillcolor=0),
+#         transforms.ToTensor(),  #Too slow
+#         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+    
+    dataset = datasets.ImageFolder(root=os.path.join(args.data_dir, 'train'),
+                                   transform=transform)
+    train_sampler = data.distributed.DistributedSampler(
+        dataset, num_replicas=int(args.world_size), rank=int(args.rank)) if args.multigpus_distributed else None
+    return data.DataLoader(dataset, batch_size=args.batch_size, shuffle=train_sampler is None,
+                                       sampler=train_sampler, collate_fn=dis_util.fast_collate, **kwargs), train_sampler
+
+
+def _get_test_data_loader(args, **kwargs):
+    logger.info("Get test data loader")
+    transform = transforms.Compose([
+        transforms.Resize((args.width, args.height)),
+#         transforms.ToTensor(),
+#         transforms.Normalize(
+#             (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+     
+    dataset = datasets.ImageFolder(root=os.path.join(args.data_dir, 'val'),
+                                   transform=transform)
+    val_sampler = data.distributed.DistributedSampler(dataset) if args.multigpus_distributed else None
+    return data.DataLoader(dataset, batch_size=args.test_batch_size, shuffle=False, 
+                           sampler=val_sampler, collate_fn=dis_util.fast_collate, **kwargs)
+
+
+def train(current_gpu, args):
+    best_acc1 = -1
+    model_history = {}
+    model_history = util.init_modelhistory(model_history)
+    train_start = time.time()
+
+    ## choose model from pytorch model_zoo
+    model = util.torch_model(args.model_name, pretrained=True)
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    ## distributed_setting 
+    model, args = dis_util.dist_setting(current_gpu, model, criterion, args)
+
+    ## CuDNN library will benchmark several algorithms and pick that which it found to be fastest
+    cudnn.benchmark = False if args.seed else True
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    if args.apex:
+        model, optimizer = dis_util.apex_init(model, optimizer, args)
+    
+    
+#     args.collate_fn = partial(dis_util.fast_collate, memory_format=args.memory_format)
+    
+    
+    train_loader, train_sampler = _get_train_data_loader(args, **args.kwargs)
+    test_loader = _get_test_data_loader(args, **args.kwargs)
+
+    logger.info("Processes {}/{} ({:.0f}%) of train data".format(
+        len(train_loader.sampler), len(train_loader.dataset),
+        100. * len(train_loader.sampler) / len(train_loader.dataset)
+    ))
+
+    logger.info("Processes {}/{} ({:.0f}%) of test data".format(
+        len(test_loader.sampler), len(test_loader.dataset),
+        100. * len(test_loader.sampler) / len(test_loader.dataset)
+    ))
+
+    for epoch in range(1, args.num_epochs + 1):
+        ## 
+        batch_time = util.AverageMeter('Time', ':6.3f')
+        data_time = util.AverageMeter('Data', ':6.3f')
+        losses = util.AverageMeter('Loss', ':.4e')
+        top1 = util.AverageMeter('Acc@1', ':6.2f')
+        top5 = util.AverageMeter('Acc@5', ':6.2f')
+        progress = util.ProgressMeter(
+            len(train_loader),
+            [batch_time, data_time, losses, top1, top5],
+            prefix="Epoch: [{}]".format(epoch))
+
+        model.train()
+        end = time.time()
+        
+        ## Set epoch count for DistributedSampler
+        if args.multigpus_distributed:
+            train_sampler.set_epoch(epoch)
+        
+        
+        prefetcher = util.data_prefetcher(train_loader)
+        input, target = prefetcher.next()
+        batch_idx = 0
+        while input is not None:
+
+            batch_idx += 1
+            
+            if args.prof >= 0 and batch_idx == args.prof:
+                print("Profiling begun at iteration {}".format(batch_idx))
+                torch.cuda.cudart().cudaProfilerStart()
+                
+            if args.prof >= 0: torch.cuda.nvtx.range_push("Body of iteration {}".format(batch_idx))
+
+            util.adjust_learning_rate(optimizer, epoch, batch_idx, len(train_loader), args)
+
+            # compute output
+            if args.prof >= 0: torch.cuda.nvtx.range_push("forward")
+            output = model(input)
+            if args.prof >= 0: torch.cuda.nvtx.range_pop()
+            loss = criterion(output, target)
+
+            # compute gradient and do SGD step
+            optimizer.zero_grad()
+            
+            if args.prof >= 0: torch.cuda.nvtx.range_push("backward")
+            if args.apex:
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()
+            if args.prof >= 0: torch.cuda.nvtx.range_pop()
+
+            if args.prof >= 0: torch.cuda.nvtx.range_push("optimizer.step()")
+            optimizer.step()
+            if args.prof >= 0: torch.cuda.nvtx.range_pop()
+
+            if True or batch_idx % args.log_interval == 0:
+                # Every print_freq iterations, check the loss, accuracy, and speed.
+                # For best performance, it doesn't make sense to print these metrics every
+                # iteration, since they incur an allreduce and some host<->device syncs.
+
+                # Measure accuracy
+                prec1, prec5 = util.accuracy(output.data, target, topk=(1, 5))
+
+                # Average loss and accuracy across processes for logging
+                if args.multigpus_distributed:
+                    reduced_loss = dis_util.reduce_tensor(loss.data, args)
+                    prec1 = dis_util.reduce_tensor(prec1, args)
+                    prec5 = dis_util.reduce_tensor(prec5, args)
+                else:
+                    reduced_loss = loss.data
+
+                # to_python_float incurs a host<->device sync
+                losses.update(to_python_float(reduced_loss), input.size(0))
+                top1.update(to_python_float(prec1), input.size(0))
+                top5.update(to_python_float(prec5), input.size(0))
+                
+                ## Waiting until finishing operations on GPU (Pytorch default: async)
+                torch.cuda.synchronize()
+                batch_time.update((time.time() - end)/args.log_interval)
+                end = time.time()
+
+                if current_gpu == 0:
+                    print('Epoch: [{0}][{1}/{2}]  '
+                          'Time {batch_time.val:.3f} ({batch_time.avg:.3f})  '
+                          'Speed {3:.3f} ({4:.3f})  '
+                          'Loss {loss.val:.10f} ({loss.avg:.4f})  '
+                          'Prec@1 {top1.val:.3f} ({top1.avg:.3f})  '
+                          'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                              epoch, batch_idx, len(train_loader),
+                              args.world_size*args.batch_size/batch_time.val,
+                              args.world_size*args.batch_size/batch_time.avg,
+                              batch_time=batch_time,
+                              loss=losses, top1=top1, top5=top5))
+                    model_history['epoch'].append(epoch)
+                    model_history['batch_idx'].append(batch_idx)
+                    model_history['batch_time'].append(batch_time.val)
+                    model_history['losses'].append(losses.val)
+                    model_history['top1'].append(top1.val)
+                    model_history['top5'].append(top5.val)
+                    
+                if args.prof >= 0: torch.cuda.nvtx.range_push("prefetcher.next()")
+                input, target = prefetcher.next()
+                if args.prof >= 0: torch.cuda.nvtx.range_pop()
+
+                # Pop range "Body of iteration {}".format(i)
+                if args.prof >= 0: torch.cuda.nvtx.range_pop()
+                    
+
+                if args.prof >= 0 and batch_idx == args.prof + 10:
+                    print("Profiling ended at iteration {}".format(batch_idx))
+                    torch.cuda.cudart().cudaProfilerStop()
+                    quit()
+               
+        acc1 = validate(test_loader, model, criterion, epoch, model_history, args)
+        
+        print(" ****  acc1 :{}".format(acc1))
+        # remember best acc@1 and save checkpoint
+        is_best = acc1 > best_acc1
+        best_acc1 = max(acc1, best_acc1)
+
+        if not args.multigpus_distributed or (args.multigpus_distributed and args.rank % args.num_gpus == 0):
+            util.save_history(os.path.join(args.output_data_dir,
+                          'model_history.p'), model_history)
+
+            util.save_model({
+                'epoch': epoch + 1,
+                'model_name': args.model_name,
+                'state_dict': model.state_dict(),
+                'best_acc1': best_acc1,
+                'optimizer': optimizer.state_dict(),
+                'class_to_idx' : train_loader.dataset.class_to_idx,
+            }, is_best, args.model_dir)
+
+
+def validate(val_loader, model, criterion, epoch, model_history, args):
+    batch_time = util.AverageMeter('Time', ':6.3f')
+    losses = util.AverageMeter('Loss', ':.4e')
+    top1 = util.AverageMeter('Acc@1', ':6.2f')
+    top5 = util.AverageMeter('Acc@5', ':6.2f')
+    progress = util.ProgressMeter(
+        len(val_loader),
+        [batch_time, losses, top1, top5],
+        prefix='Test: ')
+
+    # switch to evaluate mode
+    model.eval()
+    end = time.time()
+
+    prefetcher = util.data_prefetcher(val_loader)
+    input, target = prefetcher.next()
+    batch_idx = 0
+    while input is not None:
+        batch_idx += 1
+    
+        # compute output
+        with torch.no_grad():
+#             data = data.contiguous(memory_format=args.memory_format)
+#             target = target.contiguous()
+#             data = data.cuda(non_blocking=True)
+#             target = target.cuda(non_blocking=True)
+
+            output = model(input)
+            loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        prec1, prec5 = util.accuracy(output.data, target, topk=(1, 5))
+
+        if args.multigpus_distributed:
+            reduced_loss = dis_util.reduce_tensor(loss.data, args)
+            prec1 = dis_util.reduce_tensor(prec1, args)
+            prec5 = dis_util.reduce_tensor(prec5, args)
+        else:
+            reduced_loss = loss.data
+
+        losses.update(to_python_float(reduced_loss), input.size(0))
+        top1.update(to_python_float(prec1), input.size(0))
+        top5.update(to_python_float(prec5), input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # TODO:  Change timings to mirror train().
+        if args.current_gpu == 0 and batch_idx % args.log_interval == 0:
+            print('Test: [{0}/{1}]  '
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})  '
+                  'Speed {2:.3f} ({3:.3f})  '
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})  '
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})  '
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                      batch_idx, len(val_loader),
+                      args.world_size * args.batch_size / batch_time.val,
+                      args.world_size * args.batch_size / batch_time.avg,
+                      batch_time=batch_time, loss=losses,
+                      top1=top1, top5=top5))
+            model_history['val_epoch'].append(epoch)
+            model_history['val_batch_idx'].append(batch_idx)
+            model_history['val_batch_time'].append(batch_time.val)
+            model_history['val_losses'].append(losses.val)
+            model_history['val_top1'].append(top1.val)
+            model_history['val_top5'].append(top5.val)
+        input, target = prefetcher.next()
+
+    print('  Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+    model_history['val_avg_epoch'].append(epoch)
+    model_history['val_avg_batch_time'].append(batch_time.avg)
+    model_history['val_avg_losses'].append(losses.avg)
+    model_history['val_avg_top1'].append(top1.avg)
+    model_history['val_avg_top5'].append(top5.avg)
+    return top1.avg
+
+
+def main():
+    args = parser_args()
+    args.use_cuda = args.num_gpus > 0
+    print("args.use_cuda : {} , args.num_gpus : {}".format(
+        args.use_cuda, args.num_gpus))
+    args.kwargs = {'num_workers': 4,
+                   'pin_memory': True} if args.use_cuda else {}
+    args.device = torch.device("cuda" if args.use_cuda else "cpu")
+    dis_util.dist_init(train, args)
+
+
+if __name__ == '__main__':
+    main()
--- working_file/001.image_segmentation_pytorch/README.md
+++
@@ -1,7 +0,0 @@
-# image_segmentation
-
-<p> tensorflow 2.x 기반의 tensorflow 공식 홈페이지에서 제공하는 <strong><a href="https://www.tensorflow.org/tutorials/images/segmentation" target="_blank" class ='btn-default'>image segmentation</a></strong>을 AWS의 SageMaker에서 활용할 수 있도록 변경한 코드입니다. </p>
-<p> 기본적인 아키텍처 구조는 <strong><a href="https://arxiv.org/abs/1505.04597" target="_blank" class ='btn-default'>U-Net</a></strong> 로서 pixel-wise binary classification을 수행하게 됩니다. </p>
-<p> 최종 결과는 Accuracy와 Loss 함수를 볼 수 있으며 특정 epoch마다 저장된 mask 결과 값을 함께 볼 수 있도록 구현하였으며, inference에 대해 endpoint에서 추론할 수 있는 방법을 담고 있습니다. 추가적으로 현재 serving 환경에 없는 opencv를 사용할 수 있도록 custom docker container를 만들 수 있는 가이드도 함께 추가하였습니다.</p>
-
-
