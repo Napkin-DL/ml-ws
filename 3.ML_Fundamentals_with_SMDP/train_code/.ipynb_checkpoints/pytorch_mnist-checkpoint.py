caca05cbd22389060746d9ea2ed7b014b790d39b
youngjoon <choijoon@amazon.com>
2021-02-06 10:48:02 +0000

    update

--- 3.ML_Fundamentals_with_SMDP/train_code/.ipynb_checkpoints/pytorch_mnist-checkpoint.py
+++ 3.ML_Fundamentals_with_SMDP/train_code/.ipynb_checkpoints/pytorch_mnist-checkpoint.py
@@ -13,21 +13,6 @@ import torch.utils.data
 import torch.utils.data.distributed
 from torchvision import datasets, transforms
 
-
-########################################################
-####### 1. SageMaker Distributed Data Parallel  ########
-#######  - Import Package and Initialization    ########
-########################################################
-
-import smdistributed.dataparallel.torch.distributed as smdp
-from smdistributed.dataparallel.torch.parallel.distributed import DistributedDataParallel as smdpDDP
-
-if not smdp.is_initialized():
-    smdp.init_process_group()
-    
-#######################################################
-
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -59,18 +44,7 @@ def _get_train_data_loader(args, is_distributed, **kwargs):
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ]))
-    
-    #######################################################
-    ####### 3. SageMaker Distributed Data Parallel  #######
-    #######  - Add num_replicas and rank            #######
-    #######################################################
-
-    train_sampler = torch.utils.data.distributed.DistributedSampler(dataset,
-                                                                    num_replicas=args.world_size,
-                                                                    rank=args.rank) if is_distributed else None
-    
-    #######################################################
-    
+    train_sampler = torch.utils.data.distributed.DistributedSampler(dataset) if is_distributed else None
     return torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=train_sampler is None,
                                        sampler=train_sampler, **kwargs)
 
@@ -86,12 +60,11 @@ def _get_test_data_loader(args, **kwargs):
 
 
 def train(args):
-    is_distributed = args.world_size > 1
-    logger.debug("Distributed training - {}".format(is_distributed))
+    is_distributed = None
     use_cuda = torch.cuda.is_available()
     kwargs = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
     device = torch.device("cuda" if use_cuda else "cpu")
-
+    
     # set the seed for generating random numbers
     torch.manual_seed(args.seed)
     if use_cuda:
@@ -109,22 +82,10 @@ def train(args):
         len(test_loader.sampler), len(test_loader.dataset),
         100. * len(test_loader.sampler) / len(test_loader.dataset)
     ))
-    
-    
+
     model = Net().to(device)
-    
-    
-    #######################################################
-    ####### 4. SageMaker Distributed Data Parallel  #######
-    #######  - Add num_replicas and rank            #######
-    #######################################################    
-    
-    if is_distributed and use_cuda:
-        torch.cuda.set_device(args.local_rank)
-        model = smdpDDP(model)
-        
-    #######################################################    
-    
+    model = torch.nn.DataParallel(model)
+
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
@@ -135,15 +96,14 @@ def train(args):
             output = model(data)
             loss = F.nll_loss(output, target)
             loss.backward()
-            if is_distributed and not use_cuda:
-                # average gradients manually for multi-machine cpu case only
-                _average_gradients(model)
             optimizer.step()
+            
             if batch_idx % args.log_interval == 0:
-                logger.info('Train Epoch: {} [{}/{} ({:.0f}%)], Loss: {:.6f},'.format(
+                logger.info('Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}'.format(
                     epoch, batch_idx * len(data), len(train_loader.sampler),
                     100. * batch_idx / len(train_loader), loss.item()))
         test(model, test_loader, device)
+    
     save_model(model, args.model_dir)
 
 
@@ -163,7 +123,6 @@ def test(model, test_loader, device):
     logger.info('Test set: Average loss: {:.4f}, Accuracy: {:.0f}% ({}/{})\n'.format(
         test_loss, 100. * correct / len(test_loader.dataset), correct, len(test_loader.dataset)))
 
-
 def model_fn(model_dir):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = torch.nn.DataParallel(Net())
@@ -177,7 +136,7 @@ def save_model(model, model_dir):
     path = os.path.join(model_dir, 'model.pth')
     # recommended way from http://pytorch.org/docs/master/notes/serialization.html
     torch.save(model.cpu().state_dict(), path)
-    
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -198,9 +157,14 @@ if __name__ == '__main__':
     parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                         help='how many batches to wait before logging training status')
 
+    # Setting for Distributed Training
+    parser.add_argument('--world-size', type=int, default=1)
+    parser.add_argument('--local-rank', type=int, default=0)
+    parser.add_argument('--rank', type=int, default=0)
+    
     # SageMaker Container environment
-    parser.add_argument('--model-dir', type=str, default='./model')
-    parser.add_argument('--data-dir', type=str, default='./data')
+    parser.add_argument('--model-dir', type=str, default='../model')
+    parser.add_argument('--data-dir', type=str, default='../data')
     
     args = parser.parse_args()
     
@@ -213,16 +177,4 @@ if __name__ == '__main__':
             os.makedirs(args.model_dir)
         pass
 
-    ########################################################
-    ####### 2. SageMaker Distributed Data Parallel   #######
-    #######  - Get all number of GPU and rank number #######
-    ########################################################
-    
-    args.world_size = smdp.get_world_size()  # all number of GPU
-    args.rank = smdp.get_rank()              # total rank in all hosts
-    args.local_rank = smdp.get_local_rank()  # rank per host
-    
-    ########################################################
-    
-    
     train(args)
